5c2ff47b8ae4376ede798f545c8f03c627b379f1
Tay811 <57946873+Tay811@users.noreply.github.com>
2022-09-02 19:13:32 +0300

    Update Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+      <a className="grey-text text-lighten-4 right" href="https://github.com/Tay811/React_movies_API">Repo</a>
       </div>
     </div>
   </footer>
